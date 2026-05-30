ca8b4eb346e8d6e3461220adff9fd30965836167
Arzu <arzunusretova12@gmail.com>
2026-05-30 18:58:58 +0400

    feat: Add ci-cd pipeline for backend

---
+++ src/main/java/org/example/githubactionsdemo/controller/Controller.java
@@ -0,0 +1,13 @@
+package org.example.githubactionsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller
+{
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello Github actions";
+    }
+}
